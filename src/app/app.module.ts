9d6b3ce278f84bdf8922b944bb647a68deb2db81
Gaston Roch <gaston.roch1998@gmail.com>
2021-08-21 18:35:21 -0300

    cambios con correccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PrimerTPComponent } from './components/primerTP/primerTP.component';
+import { SegundoTPComponent } from './components/segundoTP/segundoTP.component';
+import { TercerTPComponent } from './components/tercerTP/tercerTP.component';
+
+
+
 
 @NgModule({
   declarations: [
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  
+  navbarCollapsed = true;
   constructor() { }
 
   ngOnInit(): void {
